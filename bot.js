b4ce3870370efebd77df53bef22d104712222b02
Glitch (twitch-chatbot) <none>
2021-10-13 15:58:15 +0000

    ⛰🐓 Checkpoint ./bot.js:37430677/1276

--- bot.js
+++ bot.js
@@ -13,6 +13,7 @@ const opts = {
 
 // Create a client with our options
 const client = new tmi.client(opts);
+let highestRoll = -1;
 
 // Register our event handlers (defined below)
 client.on('message', onMessageHandler);
@@ -29,19 +30,30 @@ function onMessageHandler (target, context, msg, self) {
   const commandName = msg.trim();
 
   // If the command is known, let's execute it
-  if (commandName === '!d20') {
-    const num = rollDice(commandName);
-    client.say(target, `You rolled a ${num}. Link: https://glitch.com/~twitch-chatbot`);
-    console.log(`* Executed ${commandName} command`);
+  if (commandName.startsWith('!d')) {
+    rollDice(target, commandName);
   } else {
     console.log(`* Unknown command ${commandName}`);
   }
 }
 
 // Function called when the "dice" command is issued
-function rollDice () {
-  const sides = 20;
-  return Math.floor(Math.random() * sides) + 1;
+function rollDice (target, commandName) {
+  const regex = /!d\d{1,3}\b/g;
+  const found = commandName.match(regex);
+
+  const sides = parseInt(found[0].replace("!d",""), 10);
+  
+  const result = Math.floor(Math.random() * sides) + 1;
+  
+  if(result > highestRoll) {
+    highestRoll = result;
+  }
+  
+  client.say(
+    target,
+    `You rolled a ${result} with a ${sides} sided die The highest roll so far is ${higestRoll}`
+  console.log(`* Executed ${commandName} command`); 
 }
 
 // Called every time the bot connects to Twitch chat
