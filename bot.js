fe3d02da062ecda67dc6ae39ff0b7e304bde8838
Glitch (twitch-chatbot) <none>
2021-10-14 16:13:44 +0000

    🗻🚞 Checkpoint ./bot.js:37430677/14939

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const tmi = require('tmi.js');
+const tmi = require("tmi.js");
 
 // Define configuration options
 const opts = {
@@ -6,10 +6,16 @@ const opts = {
     username: process.env.BOT_USERNAME,
     password: process.env.OAUTH_TOKEN
   },
-  channels: [
-    process.env.CHANNEL_NAME
-  ]
+  channels: [process.env.CHANNEL_NAME]
 };
+
+var low = require("lowdb");
+var FileSync = require("lowdb/adapters/FileSync");
+var adapter = new FileSync(".data/db.json");
+var db = low(adapter);
+db.defaults({ users: [
+    ]
+}).write();
 
 // Create a client with our options
 const client = new tmi.client(opts);
@@ -17,94 +23,124 @@ let highestRoll = 0;
 let diceLock;
 
 // Register our event handlers (defined below)
-client.on('message', onMessageHandler);
-client.on('connected', onConnectedHandler);
+client.on("message", onMessageHandler);
+client.on("connected", onConnectedHandler);
 
 // Connect to Twitch:
 client.connect();
 
 // Called every time a message comes in
-function onMessageHandler (target, userstate, msg, self) {
-  if (self) { return; } // Ignore messages from the bot
+function onMessageHandler(target, userstate, msg, self) {
+  if (self) {
+    return;
+  } // Ignore messages from the bot
   console.log(target);
   // Remove whitespace from chat message
   const commandName = msg.trim();
 
   // If the command is known, let's execute it
-  if (commandName.startsWith('!d')) {
+  if (commandName.startsWith("!d")) {
     rollDice(target, commandName);
-  } else if(commandName === "!resetDie") {
+  } else if (commandName === "!resetDie") {
     resetDie(target, userstate);
-  } else if (commandName.startsWith('!lock')) {
-    lockDice (target, commandName)
+  } else if (commandName.startsWith("!lock")) {
+    lockDice(target, commandName);
   } else if (commandName === "!roll") {
     rollLockedDice(target);
   } else if (commandName === "!laughTrack") {
     laughTrack(target);
   } else if (commandName === "say thank you, squizzle bot!") {
-    client.say(
-      target,
-        "\"Thank you squizzle bot!\"");
+    client.say(target, '"Thank you squizzle bot!"');
   } else if (commandName.startsWith("!throw")) {
     rockPaperScissors(target, commandName, userstate);
   } else if (commandName === "!streamerHype") {
     streamerHype(target, userstate.username);
+  } else if (commandName === "!addUser") {
+    addUser(target, userstate);
+  } else if (commandName === "!resetDb") {
+    resetDb(target, userstate);
   }
+}
+
+function addNewUser(username) {
+  db.get("users")
+    .push({ "username": username, points: 100 })
+    .write();
+  console.log("New user inserted in the database");
+}
+
+function addUser(target, userstate) {
+  if (!isMod(userstate, target)) {
+    return;
+  }
+  db.get("users")
+    .push({ firstName: "Test", lastName: "TEST" })
+    .write();
+  console.log("New user inserted in the database");
+  client.say(target, "New user inserted in the database");
+}
+
+function resetDb(target, userstate) {
+  if (!isMod(userstate, target)) {
+    return;
+  }
+  db.get('users')
+    .remove()
+    .write()
+  console.log("Database cleared");
+  client.say(target, "Database cleared");
 }
 
 function resetDie(target, userstate) {
   console.log(userstate, target);
-  if(isMod(userstate, target)) {
+  if (isMod(userstate, target)) {
     highestRoll = 0;
     client.say(
-        target,
-        `The die has been reset. The highest roll so far is ${highestRoll}`
+      target,
+      `The die has been reset. The highest roll so far is ${highestRoll}`
     );
   } else {
     client.say(
       target,
-        "BOP BOP BOP Only mods or the broadcaster can reset the die!"
-    )
+      "BOP BOP BOP Only mods or the broadcaster can reset the die!"
+    );
   }
 }
 
 // Function called when the "dice" command is issued
-function rollDice (target, commandName) {
+function rollDice(target, commandName) {
   const regex = /!d\d{1,3}\b/g;
   const found = commandName.match(regex);
 
-  const sides = parseInt(found[0].replace("!d",""), 10);
-  
+  const sides = parseInt(found[0].replace("!d", ""), 10);
+
   const result = Math.floor(Math.random() * sides) + 1;
-  
-  if(result > highestRoll) {
+
+  if (result > highestRoll) {
     highestRoll = result;
   }
-  
+
   client.say(
     target,
-    `You rolled a ${result} with a ${sides} sided die. The highest roll so far is ${highestRoll}`);
-  console.log(`* Executed ${commandName} command`); 
+    `You rolled a ${result} with a ${sides} sided die. The highest roll so far is ${highestRoll}`
+  );
+  console.log(`* Executed ${commandName} command`);
 }
 
-function lockDice (target, commandName) {
+function lockDice(target, commandName) {
   const regex = /!lock\d{1,3}\b/g;
   const found = commandName.match(regex);
 
-  const sides = parseInt(found[0].replace("!lock",""), 10);
-  
+  const sides = parseInt(found[0].replace("!lock", ""), 10);
+
   diceLock = sides;
-  client.say(
-    target,
-    `Die locked to ${sides} sides. Roll for success!`);
+  client.say(target, `Die locked to ${sides} sides. Roll for success!`);
 }
 
 function rollLockedDice(target) {
   const result = Math.floor(Math.random() * diceLock) + 1;
-  
-  client.say(
-    target,
-    `You rolled a ${result} with a ${diceLock} sided die.`);
+
+  client.say(target, `You rolled a ${result} with a ${diceLock} sided die.`);
 }
 
 function laughTrack(target) {
@@ -113,17 +149,14 @@ function laughTrack(target) {
     "@squizzleflip stop, I am not programmed to laugh this much",
     "cool story squizz"
   ];
-  
+
   const result = Math.floor(Math.random() * tracks.length);
-  
-  client.say(
-    target,
-    tracks[result]
-  );
+
+  client.say(target, tracks[result]);
 }
 
 // Called every time the bot connects to Twitch chat
-function onConnectedHandler (addr, port) {
+function onConnectedHandler(addr, port) {
   setInterval(() => {
     streamerHype("#squizzleflip", "squizzleflip");
   }, 300000);
@@ -135,8 +168,8 @@ function onConnectedHandler (addr, port) {
   console.log(`* Connected to ${addr}:${port}`);
 }
 
-function isMod(user, channel){
-  let isMod = user.mod || user['user-type'] === 'mod';
+function isMod(user, channel) {
+  let isMod = user.mod || user["user-type"] === "mod";
   let isBroadcaster = channel.slice(1) === user.username;
   let isSquizzle = user.username === "squizzleflip";
   return isMod || isBroadcaster || isSquizzle;
@@ -144,63 +177,63 @@ function isMod(user, channel){
 
 function rockPaperScissors(target, commandName, userstate) {
   const userThrow = commandName.split(" ")[1];
-  const throws = [
-    "rock",
-    "paper",
-    "scissors"
-  ];
-  
+  const throws = ["rock", "paper", "scissors"];
+
   if (!throws.includes(userThrow)) {
-      client.say(
-        target,
-        `${userThrow} is not a rock, a paper, or a scissor BOP BOP BOP`
-      );
-    
-      return;
+    client.say(
+      target,
+      `${userThrow} is not a rock, a paper, or a scissor BOP BOP BOP`
+    );
+
+    return;
   }
   
+  const player = db.get('posts')
+    .find({ username: userstate.username })
+
   const result = Math.floor(Math.random() * 3);
-  
+
   let botThrow = throws[result];
-  
+
   //from the perspective of the bot
   let matchResult;
-  
+
   if (botThrow === "rock") {
-    if(userThrow === "scissors") {
+    if (userThrow === "scissors") {
       matchResult = "win";
-    } else if(userThrow === "paper") {
+    } else if (userThrow === "paper") {
       matchResult = "lose";
-    } else if(userThrow === "rock") {
+    } else if (userThrow === "rock") {
       matchResult = "tie";
     }
   } else if (botThrow === "scissors") {
-      if (userThrow === "scissors") {
-        matchResult = "tie";
-      } else if(userThrow === "paper") {
-        matchResult = "win";
-      } else if(userThrow === "rock") {
-        matchResult = "lose";
-      }
+    if (userThrow === "scissors") {
+      matchResult = "tie";
+    } else if (userThrow === "paper") {
+      matchResult = "win";
+    } else if (userThrow === "rock") {
+      matchResult = "lose";
+    }
   } else if (botThrow === "paper") {
-      if (userThrow === "scissors") {
-        matchResult = "lose";
-      } else if(userThrow === "paper") {
-        matchResult = "tie";
-      } else if(userThrow === "rock") {
-        matchResult = "win";
-      }
+    if (userThrow === "scissors") {
+      matchResult = "lose";
+    } else if (userThrow === "paper") {
+      matchResult = "tie";
+    } else if (userThrow === "rock") {
+      matchResult = "win";
+    }
   }
-  
+
   let winMessage;
   if (matchResult === "win") {
     winMessage = "I win! Let's play again!";
   } else if (matchResult === "lose") {
-    winMessage = "I lose... INCONCEIVABLE! Why was I programmed to feel sadness?";
+    winMessage =
+      "I lose... INCONCEIVABLE! Why was I programmed to feel sadness?";
   } else {
     winMessage = "A tie! It seems that we are at an impasse...";
   }
-  
+
   client.say(
     target,
     `I threw ${botThrow}, ${userstate.username} threw ${userThrow}. ${winMessage}`
@@ -208,19 +241,14 @@ function rockPaperScissors(target, commandName, userstate) {
 }
 
 function streamerHype(target, username) {
-    const hypeMessages = [
-      `${username} is my favorite stream in the Citadel`,
-      `${username}, you are awesome!`,
-      `${username}, you got this!`,
-      `I love this game!`,
-      `What is this game? I haven't seen it before, but it looks fun!`,
-    ];
-  
-    const result = Math.floor(Math.random() * hypeMessages.length);
-    client.say(
-      target,
-      `${hypeMessages[result]}`
-    );
+  const hypeMessages = [
+    `${username} is my favorite stream in the Citadel`,
+    `${username}, you are awesome!`,
+    `${username}, you got this!`,
+    `I love this game!`,
+    `What is this game? I haven't seen it before, but it looks fun!`
+  ];
+
+  const result = Math.floor(Math.random() * hypeMessages.length);
+  client.say(target, `${hypeMessages[result]}`);
 }
-
-
